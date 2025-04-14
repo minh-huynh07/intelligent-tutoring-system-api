d82f2587d957865422c911e43a3e54d2ed91563e
Phu Nguyen <dinhphu951998@gmail.com>
2025-04-15 00:51:33 +0700

    crawl data

--- Crawling/Crawler.py
+++ Crawling/Crawler.py
@@ -3,6 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 from nltk import ngrams, WordNetLemmatizer
 
+from utils import create_folder
+
 base_url = "https://dota2.fandom.com"
 
 def get_html(url):
@@ -61,6 +63,7 @@ def save_text_file(name, content):
 
 
 # Run
+create_folder("data")
 heroes = get_all_heroes()
 for hero in heroes:
     print(f"Fetching data for {hero['name']}...")
@@ -68,18 +71,18 @@ for hero in heroes:
 
     save_text_file(f'data/{hero["name"]}.txt', abilities)
     print(f"Data for {hero['name']} saved.")
-
-    # Save normalized text
-    norm = normalize_text(abilities)
-    # Calculate TF-IDF for normalized text
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform([norm])
-
-    # Save TF-IDF values to a file
-    tfidf_values = "\n".join(
-        f"{word}: {tfidf_matrix[0, idx]:.4f}" for word, idx in vectorizer.vocabulary_.items()
-    )
-    save_text_file(f'preprocessing/{hero["name"]}_tfidf.txt', tfidf_values)
+    #
+    # # Save normalized text
+    # norm = normalize_text(abilities)
+    # # Calculate TF-IDF for normalized text
+    # vectorizer = TfidfVectorizer()
+    # tfidf_matrix = vectorizer.fit_transform([norm])
+    #
+    # # Save TF-IDF values to a file
+    # tfidf_values = "\n".join(
+    #     f"{word}: {tfidf_matrix[0, idx]:.4f}" for word, idx in vectorizer.vocabulary_.items()
+    # )
+    # save_text_file(f'preprocessing/{hero["name"]}_tfidf.txt', tfidf_values)
 
 
 
@@ -91,5 +94,6 @@ for hero in heroes:
     # all_grams = list(set(uni_grams + bi_grams + tri_grams))
     # all_grams = ["\r\n".join(gram) for gram in all_grams]
 
-    save_text_file(f'preprocessing/{hero["name"]}.txt', all_grams)
-    break
+    # save_text_file(f'preprocessing/{hero["name"]}.txt', all_grams)
+
+print(f"Total heroes found: {len(heroes)}")
--- Crawling/StatsCrawler.py
+++ Crawling/StatsCrawler.py
@@ -1,4 +1,9 @@
 import requests
+import pandas as pd
+import json
+import time
+
+from utils import create_folder
 
 HEROES_URL = "https://api.opendota.com/api/heroes"
 BENCHMARK_BY_HERO_ID_URL = "https://api.opendota.com/api/benchmarks" # ?hero_id=1
@@ -7,32 +12,60 @@ HEROES_STATS_URL = "https://api.opendota.com/api/heroStats"
 def get_json(url):
     response = requests.get(url)
 
+    retry_count = 0
+    while response.status_code == 429 and retry_count < 5:
+        time.sleep(60)
+        retry_count += 1
+        response = requests.get(url)
+
     if response.status_code == 200:
         return response.json()
     else:
-        raise Exception(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
+        return None
+
+
+def get_percentile(arr, percentile):
+    expected_percentile = next(filter(lambda x: x["percentile"] == percentile, arr), None)
+    return expected_percentile["value"] if expected_percentile else ""
 
 def calculate_benchmark(benchmark):
-    # Calculate the benchmark based on the provided data
-    # This is a placeholder function. You can implement your own logic here.
     return {
-        "hero_id": benchmark["hero_id"],
-        "games_played": benchmark["games_played"],
-        "win_rate": benchmark["win_rate"],
-        "kda": benchmark["kda"],
-        "gold_per_minute": benchmark["gold_per_minute"],
-        "xp_per_minute": benchmark["xp_per_minute"]
+        "gold_per_min": get_percentile(benchmark["gold_per_min"], 0.5),
+        "xp_per_min": get_percentile(benchmark["xp_per_min"], 0.5),
+        "kills_per_min": get_percentile(benchmark["kills_per_min"], 0.5),
+        "last_hits_per_min": get_percentile(benchmark["last_hits_per_min"], 0.5),
+        "hero_damage_per_min": get_percentile(benchmark["hero_damage_per_min"], 0.5),
+        "hero_healing_per_min": get_percentile(benchmark["hero_healing_per_min"], 0.5),
+        "tower_damage": get_percentile(benchmark["tower_damage"], 0.5),
     }
 
-data = []
+def get_hero_stats():
+    arr = get_json(HEROES_STATS_URL)
+    for stat in arr:
+        for key, value in stat.items():
+            if isinstance(value, list):
+                stat[key] = ",".join(map(str, value))
+    return arr
 
 heros = get_json(HEROES_URL)
-heroes_stats = get_json(HEROES_STATS_URL)
+heroes_stats = get_hero_stats()
 
 for hero in heros:
+
     hero_id = hero["id"]
     hero_name = hero["localized_name"]
     print(f"Fetching data for {hero_name}...")
-    hero_stats = get_json(f"{BENCHMARK_BY_HERO_ID_URL}?hero_id={hero_id}")
-    heroes_stats.append(hero_stats)
-    print(f"Data for {hero_name} saved.")
+
+    benchmark_data = get_json(f"{BENCHMARK_BY_HERO_ID_URL}?hero_id={hero_id}")
+    if not benchmark_data:
+        break
+    hero_benchmark = calculate_benchmark(benchmark_data["result"])
+
+    hero_stat = next(filter(lambda x: x["id"] == hero_id, heroes_stats), None)
+    if hero_stat:
+        hero_stat.update(hero_benchmark)
+
+create_folder("stats")
+df = pd.DataFrame(heroes_stats)
+df.to_csv("stats/heroes_stats.csv", index=False)
+
